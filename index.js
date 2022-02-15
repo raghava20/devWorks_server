68f86352f3482ca0ced84fbcfc0e1aa09aa97bb7
raghava20 <raghavaanand22@gmail.com>
2022-02-15 21:54:36 +0530

    made routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from "dotenv"
 import cors from "cors"
 import { mongo } from "./mongoConnection.js"
+import { authenticationRouter } from "./routers/authenticationRouter.js"
+import { postsRouter } from "./routers/postsRouter.js"
+import { profilesRouter } from "./routers/profilesRouter.js"
 
 dotenv.config()
 
@@ -16,5 +19,9 @@ mongo()
 app.get('/', (req, res) => {
     res.send("You are listening the dev works api!")
 })
+
+app.use("/", authenticationRouter)
+app.use("/", postsRouter)
+app.use("/", profilesRouter)
 
 app.listen(PORT, () => console.log("App is running on " + PORT))
